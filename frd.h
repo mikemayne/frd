a97014392e66f12f69020cb78a31055670b5d58a
Mike Mayne <mike@stonepower.co>
2019-05-01 17:42:37 +0100

    First commit.  std::filesystem not tested on gcc8 yet.

---
+++ frd.h
@@ -0,0 +1,127 @@
+// utilities for dealing with FRD loudspeaker measurements in text files
+
+// Copyright 2019 Michael Mayne
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+// software and associated documentation files (the "Software"), to deal in the Software without restriction, 
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+// subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+// THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+// OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#include <istream>
+#include <vector>
+#include <sstream>
+#include <stdexcept>    // std::invalid_argument
+#include <algorithm>    // std::lower_bound
+
+#include <experimental/filesystem> // gcc 7
+namespace filesystem = std::experimental::filesystem; // gcc 7
+
+// not tested yet:
+// #include <filesystem> // gcc 8
+// namespace filesystem = std::filesystem; // gcc 8
+
+namespace frd {
+
+// A single data point from an FRD file
+template <typename FloatType>
+struct FRDValue
+{
+    FloatType freqHz;   // w?
+    FloatType dBSPL;    // real Pa?  see what works 
+    FloatType phaseDeg; // imag Pa?  with the calculator
+};
+
+template <typename FloatType>
+std::string str(const FRDValue<FloatType>& value)
+{
+    std::ostringstream oss;
+    oss << value.freqHz << "Hz " << value.dBSPL << "dB " << value.phaseDeg << "deg";
+    return oss.str();
+}
+
+// parses data that looks like this:
+// Freq [Hz]       dBSPL           Phase [Deg]
+// 10.00           75.18           83.73
+//
+// and returns a std::vector of FRDValues
+// treats any line that starts with a non-numeric character as a comment
+// throws std::invalid_argument if any of the data lines cannot be parsed
+template<typename FloatType>
+std::vector<FRDValue<FloatType>> parse(std::istream& text)
+{
+    std::vector<FRDValue<FloatType>> frd;
+    std::string buffer;
+    while (std::getline(text, buffer))
+    {
+        if (isalpha(buffer[0])) // comment line
+            continue;
+        
+        std::stringstream ss(buffer);
+        FRDValue<FloatType> frdValue;
+
+        if (!(ss >> frdValue.freqHz >> frdValue.dBSPL >> frdValue.phaseDeg))
+        {
+            throw std::invalid_argument("Invalid input in line: " + buffer);
+        }
+        frd.push_back(frdValue);
+    }
+    return frd;
+}
+
+template<typename FloatType>
+struct less_than_freq
+{
+    inline bool operator() (const FRDValue<FloatType>& frd1, const FRDValue<FloatType>& frd2)
+    {
+        return (frd1.freqHz < frd2.freqHz);
+    }
+};
+
+// Takes a vector of FRDValues sorted by Frequency and a frequency 
+//
+// returns the an iterator to the FRDValue 
+// where FRDValue.freqHz compares less than freq.
+//
+// If findFreq(frd, freq) == frd.end() the frequency was not found
+template<typename FloatType>
+typename std::vector<FRDValue<FloatType>>::iterator
+findFreq(std::vector<FRDValue<FloatType>>& Frd, FloatType freq)
+{   
+    FRDValue<FloatType> freqHelper {freq, static_cast<FloatType>(0), static_cast<FloatType>(0)};
+    return std::lower_bound(Frd.begin(), Frd.end(), freqHelper, less_than_freq<FloatType>());
+}
+
+// PolarData holds the dB spl, phase data for all frequencies
+// Its not used in the critical path
+template<typename FloatType>
+using PolarData = std::vector<std::vector<FRDValue<FloatType>>>;
+
+// Imports each file as a vector of FRDValues
+// Will import them in alphabetical order, make sure the files are named correctly
+template<typename FloatType>
+PolarData<FloatType> import_polardata(std::string folder)
+{
+    auto it = filesystem::directory_iterator(folder);
+    std::vector<filesystem::directory_entry> files;
+    std::copy(filesystem::begin(it), filesystem::end(it), std::back_inserter(files));
+    std::sort(files.begin(), files.end());
+
+    PolarData<FloatType> p;
+
+    for (const auto & file : files)
+    {
+        std::ifstream fs(file.path());
+        p.push_back(parse<FloatType>(fs));
+    }
+
+    return p;
+}
+
+}; // frd
---
+++ frd_test.cpp
@@ -0,0 +1,175 @@
+// Copyright 2019 Michael Mayne
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+// software and associated documentation files (the "Software"), to deal in the Software without restriction, 
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, 
+// subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO 
+// THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
+// OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#include "frd.h"
+
+#include <sstream>
+#include <fstream>
+
+#include "gtest/gtest.h"
+namespace {
+
+
+    template <typename FloatType>
+    bool equals(const frd::FRDValue<FloatType>& lhs, const frd::FRDValue<FloatType>& rhs)
+    {
+        return lhs.freqHz   == rhs.freqHz 
+            && lhs.dBSPL    == rhs.dBSPL 
+            && lhs.phaseDeg == rhs.phaseDeg;
+    }
+
+
+    TEST(FrdParseFileTest, TestParseFile) {
+
+        std::ifstream fs("testfrd.txt");
+        auto Frd = frd::parse<double>(fs);
+
+        ASSERT_EQ(256, Frd.size());
+        EXPECT_TRUE(equals(Frd[0], {10.00, 95.24, -37.18}));
+    }
+
+    TEST(FrdParseTest, TestEmpty) {
+
+        std::stringstream ss;
+        auto vec = frd::parse<double>(ss);
+
+        EXPECT_EQ(0, vec.size());
+    }
+
+    TEST(FrdParseTest, TestSingleCommentLine) {
+
+        std::stringstream ss;
+        ss << "A comment";
+        auto vec = frd::parse<double>(ss);
+
+        EXPECT_EQ(0, vec.size());
+    }
+
+    TEST(FrdParseTest, TestSingleDataLine) {
+
+        std::stringstream ss;
+        ss << "A comment" << std::endl;
+        ss << "10 1 1" << std::endl;
+        auto vec = frd::parse<double>(ss);
+
+        ASSERT_EQ(1, vec.size());
+        EXPECT_EQ(vec[0].freqHz, 10.);
+        EXPECT_EQ(vec[0].dBSPL, 1.);
+        EXPECT_EQ(vec[0].phaseDeg, 1.);
+    }
+
+    TEST(FrdParseTest, TestSingleDataLineWithLotsOfDecimalPlaces) {
+
+        std::stringstream ss;
+        ss << "A comment" << std::endl;
+        ss << "10 1 1.";
+
+        const int num_dp = 20;
+        for (int i=0; i<num_dp; ++i)
+            ss << "1";
+
+        ss << std::endl;
+
+        auto vec = frd::parse<double>(ss);
+
+        ASSERT_EQ(1, vec.size());
+        EXPECT_EQ(vec[0].freqHz, 10.);
+        EXPECT_EQ(vec[0].dBSPL, 1.);
+        EXPECT_EQ(vec[0].phaseDeg, 1.11111111111111111111);
+    }
+
+    TEST(FrdParseTest, TestMultipleDataLinesWithMixedWhitespace) {
+
+        std::stringstream ss;
+        ss << "A comment" << std::endl;
+        const int num_lines = 1000;
+        for (int i=0; i<num_lines; ++i)
+            ss << "10.0             1.0\t\t\t1.0" << std::endl;
+
+        auto vec = frd::parse<double>(ss);
+
+        ASSERT_EQ(num_lines, vec.size());
+        EXPECT_EQ(vec[0].freqHz, 10.);
+        EXPECT_EQ(vec[0].dBSPL, 1.);
+        EXPECT_EQ(vec[0].phaseDeg, 1.);
+
+        const int last_line = num_lines-1;
+        EXPECT_EQ(vec[last_line].freqHz, 10.);
+        EXPECT_EQ(vec[last_line].dBSPL, 1.);
+        EXPECT_EQ(vec[last_line].phaseDeg, 1.);
+    }
+
+    TEST(FrdFindFreqTest, TestFindInEmptyVector) {
+
+        std::vector<frd::FRDValue<double>> Frd;
+        EXPECT_EQ(findFreq(Frd, 0.), Frd.end());
+    }
+
+    TEST(FrdFindFreqTest, TestFindInSingleElementVector) {
+
+        std::vector<frd::FRDValue<double>> Frd;
+        Frd.push_back({10., 10., 10.});
+        ASSERT_NE(findFreq(Frd, 0.), Frd.end());
+        EXPECT_EQ((*findFreq(Frd, 0.)).freqHz, 10.);
+    }
+
+    TEST(FrdFindFreqTest, TestFindInMultipleElementVector) {
+
+        std::vector<frd::FRDValue<double>> Frd;
+
+        for (int i=0; i<10; ++i) {
+            Frd.push_back({i*10., i*10., 0.});
+        }
+
+        auto lessThanZero = findFreq(Frd, -10.);
+        ASSERT_NE(lessThanZero, Frd.end());
+        EXPECT_EQ((*lessThanZero).freqHz, 0.);
+
+        auto justUnderMiddle = findFreq(Frd, 45.);
+        ASSERT_NE(justUnderMiddle, Frd.end());
+        EXPECT_EQ((*justUnderMiddle).freqHz, 50.);
+
+        auto middle = findFreq(Frd, 50.);
+        ASSERT_NE(middle, Frd.end());
+        EXPECT_EQ((*middle).freqHz, 50.);
+
+        auto justOverMiddle = findFreq(Frd, 55.);
+        ASSERT_NE(justOverMiddle, Frd.end());
+        EXPECT_EQ((*justOverMiddle).freqHz, 60.);
+
+        auto last = findFreq(Frd, 90.);
+        ASSERT_NE(last, Frd.end());
+        EXPECT_EQ((*last).freqHz, 90.);
+
+        auto pastEnd = findFreq(Frd, 100.);
+        ASSERT_EQ(pastEnd, Frd.end());
+    }
+
+
+    TEST(FrdPolarDataTest, TestImportPolarData) {
+
+        auto polar = frd::import_polardata<double>("./polar_test");
+        EXPECT_EQ(polar[0][0].freqHz, 10);
+        EXPECT_EQ(polar[0][0].dBSPL, 1);
+        EXPECT_EQ(polar[0][0].phaseDeg, 1);
+
+        EXPECT_EQ(polar[1][0].freqHz, 10);
+        EXPECT_EQ(polar[1][0].dBSPL, 2);
+        EXPECT_EQ(polar[1][0].phaseDeg, 2);
+
+        EXPECT_EQ(polar[2][0].freqHz, 10);
+        EXPECT_EQ(polar[2][0].dBSPL, 3);
+        EXPECT_EQ(polar[2][0].phaseDeg, 3);
+    }
+}
